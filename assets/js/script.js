c6c12bf4fe1bbbc6cada6b02bac60bce4876a4a1
Christine Baxter <christine.d.baxter@live.com>
2021-10-16 20:42:01 -0500

    add class and methods/properties

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,43 @@
+//CREATE QUIZ CLASS
+class Quiz {
+    constructor(questions) {
+        this.score = 0;
+        this.questions = questions;
+        this.questionIndex = 0;
+    }
+
+    getQuestionIndex() {
+        return this.questions(this.questionIndex);
+    }
+
+    guess(answer) {
+        if (this.questionIndex().isCorrectAnswer(answer)) {
+            this.score++;            
+        }
+        this.questionIndex++;
+    }
+
+    isEnded() {
+        return this.questionIndex === this.questions.length;
+    }
+}
+
+//CREATE A QUESTION CLASS
+class Question {
+    constructor(text, choices, answer) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+    }
+
+    //property
+    isCorrectAnswer(choice) {
+        return this.answer === choice;
+    }
+}
+
+
+
 // Selecting required elements
 var timeEl = document.querySelector("#hdrTimerSec");
 var startBtnEl = document.querySelector("#start");
