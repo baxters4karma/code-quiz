d0c0465470d1c34583653ef92243b3d6c548d57d
Christine Baxter <christine.d.baxter@live.com>
2021-10-16 22:27:37 -0500

    practicing with training video coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,139 +36,167 @@ class Question {
     }
 }
 
-
-
-// Selecting required elements
-var timeEl = document.querySelector("#hdrTimerSec");
-var startBtnEl = document.querySelector("#start");
-var quizIntroEl = document.getElementById("quizIntro");
-var quizBoxEl = document.querySelector("#quizBox");
-var optionListEl = document.querySelectorAll("#quizBoxOptions");
-var userSelection = "";
-var timeValue = 60;
-var userScore = 0;
-
-
-//Attach event listener for Start Quiz button
-startBtnEl.addEventListener("click", function () {
-    countdown(); //starting time countdown
-    quizIntroEl.style.display = "none"; //hiding intro text 
-    showContent(); //calling showContent function for questions/choices    
-});
-
-
-quizBoxEl.addEventListener("click", function () {
-    console.log(optionListEl.outerHTML);
-    console.log(questions.options);
-    if (optionListEl.outerHTML == questions.options) {
-        userSelection = optionListEl.outerHTML;
+// DISPLAY QUESTION
+function displayQuestion() {
+    if (quiz.isEnded) {
+        showScores();
     } else {
-        userSelection = "wrong";        
-    };    
-    
-    getUserSelection(userSelection);
-    
-});
+        // show question
+        let questionEl = document.getElementById("question");
+        questionEl.innerHTML = quiz.getQuestionIndex().text;
 
-function countdown() {
-    // Timer that counts down from 60
-    var timeLeft = timeValue;
-
-    var timeInterval = setInterval(function () {
-        // As long as the time left is greater than 1
-        if (timeLeft > 1) {
-            // Set the textContent of timerEl to show the remaining seconds            
-            timeEl.textContent = timeLeft;
-            // Decrement timeLeft by 1
-            timeLeft--;
-        } else {
-            // Once timeLeft gets to 0, set timeEl to an empty string
-            timeEl.textContent = "";
-            // Use clearInterval() to stop the timer
-            clearInterval(timeInterval);            
+        // show options
+        let choices = quiz.getQuestionIndex().choices;
+        for (let i = 0; i < choices.length; i++) {
+            let choiceEl = document.getElementById("choice" + i);
+            choiceEl.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);            
         }
-    }, 1000);
-}
 
-function showContent() {        
-    for (var i = 0; i < questions.length; i++) {
-      let quizNumber = questions[i];
-      let quizBoxEl = document.getElementById("quizBox");
-      quizBoxEl[i] = document.createElement("div");
-      quizBoxEl[i].id = "quizBoxCard";
-      quizBoxEl[i].className = "quizBoxCard";
-      quizBoxEl.appendChild(quizBoxEl[i]);
-
-      let quizBoxCardEl = document.getElementById("quizBoxCard");
-      quizBoxCardEl[i] = document.createElement("div");
-      quizBoxCardEl[i].id = "quizBoxGroup";
-      quizBoxCardEl[i].className = "quizBoxGroup";
-      quizBoxCardEl.appendChild(quizBoxCardEl[i]);
-
-      let quizBoxContentEl = document.getElementById("quizBoxGroup");
-      quizBoxContentEl[i] = document.createElement("div");
-      quizBoxContentEl[i].id = "quizBoxContent";
-      quizBoxContentEl[i].className = "quizBoxContent";
-      quizBoxContentEl[i].innerHTML =
-        "<h3 id='quizBoxQuestion' class='quizBoxQuestion'>" +
-        quizNumber.question +
-        "</h3>";
-      quizBoxContentEl.appendChild(quizBoxContentEl[i]);
-
-      let quizNumberId = i;
-     
-      showOptions(quizNumberId);
-    }    
-}
-
-function showOptions(number) {
-    let questionNumber = number;
-    let quizNumber = questions[questionNumber];
-    let quizBoxEl = document.getElementById("quizBox");    
-    let quizBoxContentEl = document.getElementById("quizBoxGroup");
-    // const option = optionList.querySelectorAll(".option");
-    
-    if (questionNumber === quizNumber.numberId) {
-        for (var j = 0; j < quizNumber.options.length; j++) {
-            quizBoxEl[j] = document.createElement("div");
-            quizBoxEl[j].id = "quizBoxOptions";
-            quizBoxEl[j].className = "quizBoxOptions";
-            quizBoxEl[j].innerHTML =
-                "<input type='button' id='quizBoxOption' class='quizBoxOption'>" +
-                quizNumber.options[j] +
-                "</input>";
-            let quizBoxOptionEl = quizBoxEl[j];
-            quizBoxContentEl.appendChild(quizBoxOptionEl);
-            // option[j].setAttribute("onclick", "optionSelected(this)");
-        }
+        showProgress();
     }
 };
 
-function getUserSelection(userSelection) {
-    
-    let userAnswer = userSelection; //getting user selected option
-    let correctAnswer = questions.answer;  //getting correct answer from array
-
-    if (userAnswer == correctAnswer) {
-        //if user selected option equal to array's correct answer
-        userScore += 10;  //adding 10 to time/score value
-        console.log("Correct Answer");        
-    } else {
-        userScore -= 10; //subtracting 10 from time/score value
-        console.log("Wrong Answer");
+// create quess function
+function guess(id, guess) {
+    let button = document.getElementById(id)
+    button.onclick = function () {
+        quiz.guess(guess);
+        displayQuestion();
     }
+};
 
-    if (userScore === 0) {
-        showResult();
-    } else {
-        showContent();
-    }
-
+// create show progress
+function showProgress() {
+    let currentQuestionNumber = quiz.questionIndex + 1;
+    let progressEl = document.getElementById("progress");
+    progressEl.innerHTML =
+        'Question ${currentQuestionNumber} of ${quiz.questions.length}';
 }
 
-function showResult() {    
-    quizBoxCardEl.style.display = "none";
-    resultBox.style.display = "block";
-    const scoreText = resultBox.querySelector(".scoreText");
-
+// show score
+function showScores() {
+    let quizEndHTML =
+        '
+            <h1>All Done!</h1>
+            <h2 id="score">Your final score is ${quiz.score} of ${quiz.questions.length}</h2>
+            <div class="quizRepeat">
+                <a href="index.html">Take Quiz Again</a>
+            </div>
+        ';
+    let quizEl = document.getElementById("quiz");
+    quizEl.innerHTML = quizEndHTML;
 }
+
+// create quiz questions
+let questions = [
+    new Question(
+        "What does HTML stand for?",
+        ["Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"],
+        "Hyper Text Markup Language"
+    ),
+    new Question(
+        "What does CSS stand form?",
+        ["Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"],
+        "Cascading Style Sheet"
+    ),
+    new Question(
+        "What does PHP stand for",
+        ["Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor"],
+        "Hypertext Preprocessor"
+    ),
+    new Question(
+        "What does SQL stand for?",
+        ["Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"],
+        "Structured Query Language"
+    ),
+    new Question(
+        "What does XML stand for?",
+        ["eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"],
+        "eXtensible Markup Language"
+    ),
+    new Question(
+        "What are JavaScript Data Types",
+        ["number, string, boolean, function, object, undefined, lists",
+            "number, string, boolean, function, tuple, undefined",
+            "number, string, boolean, function, object, undefined",
+            "number, string, boolean, function, object, float"],
+        "number, string, boolean, function, object, undefined"
+    ),
+    new Question(
+        "Select the strict equality operator.",
+        ["equals",
+            "=",
+            "==",
+            "==="],
+        "==="
+    ),
+    new Question(
+        "What would be the result of 3+2'7'?",
+        ["12",
+            "57",
+            "327",
+            "5+7"],
+        "57"
+    ),
+    new Question(
+        "Select item below that is NOT a JavaScript error type",
+        ["Load time errors",
+            "Run time errors",
+            "Syntax errors",
+            "Logical errors"],
+        "Syntax errors"
+    ),
+    new Question(
+        "What does NaN stand for?",
+        ["Not a number",
+            "Not applicable now",
+            "Non-ASCII Notation",
+            "Not a negative"],
+        "Not a number"
+    )
+];
+
+let quiz = new Quiz(questions);
+
+//display question
+displayQuestion();
+
+//add a count down
+let time = 10;
+let quizTimeInMinutes = time * 60 *60;
+let quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("countdown");
+
+function startCountdown() {
+    let quizTimer = setInterval(function () {
+        if (quizTime <= 0) {
+            clearInterval(quizTimer);
+            showScores();
+        } else {
+            quizTime--;
+            let seconds = Math.floor(quizTime % 60);
+            let minutes = Math.floor(quizTime / 60) % 60;
+            counting.innerHTML = 'Time:  ${minutes} : ${seconds}';
+        }
+    }, 1000) 
+}
+
+startCountdown();
+
